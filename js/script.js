929ef4ecf9f44a57b0d1ecc5f8d785bbda223a44
f4t4l777 <rangel0011@outlook.com>
2023-10-15 11:47:51 -0300

    feat: added function to filter and protect eval()

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
 /* GET TEXT RESULT */
 const calcMathResult = document.getElementById('calcmath-result');
 
+const textsAllowed = [
+    '%',
+    '÷',
+    '*',
+    '-',
+    '+',
+    '.',
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9'
+];
+
 /* FUNCTION TO INSERT NUMBER ON TEXT RESULT */
 const insertNumber = (newNumber) => {
 
@@ -22,15 +41,43 @@ const clearNumbers = () => calcMathResult.innerText = "0";
 
 /* FUNCTION TO BACK NUMBER */
 const backNumber = () => {
-    let currentNumber = getCurrentNumber();
+    if(getCurrentNumber().length <= 1) return calcMathResult.innerText = "0";
 
-    if(currentNumber.length <= 1) return calcMathResult.innerText = "0";
+    calcMathResult.innerText = getCurrentNumber().substring(0, getCurrentNumber().length -1);
+}
 
-    calcMathResult.innerText = currentNumber.substring(0, currentNumber.length -1);
+/* FUNCTION TO APPLY PORCENT */
+const applyPorcent = () =>{
+
+}
+
+/* FUNCTION TO DIVIDE NUMBER */
+const divideNumber = () =>{
+
+}
+
+/* FUNCTION TO MULTIPLY NUMBER */
+const multiplyNumber = () =>{
+    calcMathResult.innerText = getCurrentNumber() + "*";
+}
+
+/* FUNCTION TO  */
+const calculateNumbers = () =>{
+    const filteredText = blockTextEval(getCurrentNumber());
+
+    if(filteredText.length <= 2) return calcMathResult.innerText = "0";
+
+    calcMathResult.innerText = eval(filteredText);
 }
 
 /* FUNCTION TO GET CURRENT NUMBER ON TEXT RESULT */
 function getCurrentNumber(){
     let currentNumber = calcMathResult.innerText;
     return currentNumber;
+}
+
+/*  */
+function blockTextEval(text) {
+    const filteredText = text.split('').filter(char => textsAllowed.includes(char)).join('');
+    return filteredText;
 }
