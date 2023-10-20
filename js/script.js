6b8c5b908482e5f47755e672166fbcd6b447fcb2
f4t4l777 <rangel0011@outlook.com>
2023-10-19 21:36:50 -0300

    feat: function to filter eval, and filter acents

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /* GET TEXT RESULT */
 const calcMathResult = document.getElementById('calcmath-result');
 
+/* ARRAY WITH CHARACTERS ALLOWED ON TEXT EVAL() */
 const textsAllowed = [
     '%',
     '/',
@@ -20,8 +21,26 @@ const textsAllowed = [
     '9'
 ];
 
+/* ARRAY WITH CHARACTERS */
+const textAcent = [
+    '%',
+    '/',
+    '*',
+    '-',
+    '+',
+    '.',
+];
+
+/* VARIABLE TO RESET WHEN CONSULTED RESULT */
+let wasConsulted = false;
+
 /* FUNCTION TO INSERT NUMBER ON TEXT RESULT */
 const insert = (newNumber) => {
+
+    if(wasConsulted){
+        wasConsulted = !wasConsulted;
+        return calcMathResult.innerText = newNumber;
+    }
 
     let currentNumber = getCurrentNumber();
 
@@ -29,11 +48,47 @@ const insert = (newNumber) => {
         return;
     }
 
-    if(currentNumber === "0"){
-        currentNumber = "";
+    if(currentNumber === "0" && newNumber !== "."){
+       currentNumber = "";
     }
 
-    calcMathResult.innerText = currentNumber + newNumber;
+    setTimeout(function(){
+        filterTextAcent()
+    }, 100);
+
+    const filterTextAcent = () => {
+        if (textAcent.includes(newNumber) && !currentNumber.includes(newNumber)) {
+            let hasReplaced = false;
+    
+            for (let n = 0; n < textAcent.length; n++) {
+                if (currentNumber.includes(textAcent[n]) && textAcent[n] !== newNumber) {
+                    currentNumber = currentNumber.replace(textAcent[n], newNumber);
+                    hasReplaced = true;
+                    console.log(currentNumber);
+                    calcMathResult.innerText = currentNumber;
+                    return console.log("Mais de 1");
+                }
+            }
+    
+            if (!hasReplaced) {
+                if (!currentNumber.includes(newNumber)) {
+                    currentNumber += newNumber;
+                    calcMathResult.innerText = currentNumber;
+                    console.log("Only 1");
+                } else {
+                    console.log("newNumber igual ao que já está em currentNumber. Bloqueado.");
+                }
+            }
+        } else if (/[0-9]/.test(newNumber)) {
+            // Permita números de 0 a 9
+            currentNumber += newNumber;
+            calcMathResult.innerText = currentNumber;
+            console.log("Número adicionado.");
+        } else {
+            console.log("Caractere inválido.");
+        }
+    }
+
 }
 
 /* FUNCTION TO CLEAN NUMBERS */
@@ -48,11 +103,13 @@ const backNumber = () => {
 
 /* FUNCTION TO  */
 const calculateNumbers = () =>{
-    const filteredText = blockTextEval(getCurrentNumber());
+    const filteredText = filterTextEval(getCurrentNumber());
 
     if(filteredText.length <= 2) return calcMathResult.innerText = "0";
 
     calcMathResult.innerText = eval(filteredText);
+
+    wasConsulted = true;
 }
 
 /* FUNCTION TO GET CURRENT NUMBER ON TEXT RESULT */
@@ -62,7 +119,8 @@ function getCurrentNumber(){
 }
 
 /*  */
-function blockTextEval(text) {
+function filterTextEval(text) {
     const filteredText = text.split('').filter(char => textsAllowed.includes(char)).join('');
     return filteredText;
 }
+
